79846a2a9056c3ea6217c8c9e4bce32b6011eb58
Paralock <nathanael.mercaldo@gmail.com>
2022-06-04 17:27:37 -0500

    Initial commit

---
+++ setup-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Copy to /etc/profile.d/
+
+if [ ! -f /tmp/.loggedin ]
+then
+    id=$(id -u $user)
+    export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$id/bus
+    sudo systemctl start user@$id
+    sudo touch /tmp/.loggedin
+fi

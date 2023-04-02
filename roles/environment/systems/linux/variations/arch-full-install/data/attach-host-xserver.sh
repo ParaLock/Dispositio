74ca8d3f9750989fa9b39746a8ebf0414d2d5cec
Paralock <nathanael.mercaldo@gmail.com>
2023-04-02 03:42:27 -0400

    Made some final fixes

--- roles/environment/systems/linux/variations/arch-full-install/data/attach-host-xserver.sh
+++
@@ -1,2 +0,0 @@
-sudo mkdir -p /tmp/.X11-unix/
-sudo mount --bind /mnt/.X11-unix/ /tmp/.X11-unix/
--- roles/environment/systems/linux/variations/arch-full-install/data/env-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-user={{container_username}}
-id=$(id -u $user)
-export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$id/bus
-sudo systemctl start user@$id 
-sudo rm -f /run/user/1000/pipewire-0
-sudo ln -s /mnt/pipewire-0 /run/user/1000/pipewire-0
-export DISPLAY=:0
-    
